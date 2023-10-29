2aba0411f1151829f1094d874ce465833686cf30
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-29 19:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function twoSum(input1,input2){
 	if(!isNaN(n1) && !isNaN(n2)) 
 	{
 		let totalSum = n1 + n2 ;
-		let thenSum = " The sum of " + input1 + " and " + input2 + " is " + totalSum + " . ";
+		let thenSum = " The sum of " + input1 + " and " + input2 + " is " + totalSum + ".";
 		return thenSum;
 	} 
 	else 
-	{
+	{  
 		return "Invalid input. Please enter a valid number." ;
 	}
 }
